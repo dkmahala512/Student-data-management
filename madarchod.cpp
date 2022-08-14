4f5a8adbe38513b31398f2737007277962e778ed
dkmahala512 <111015087+dkmahala512@users.noreply.github.com>
2022-08-14 20:33:25 +0530

    Create madarchod.cpp

---
+++ madarchod.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  cout<<"Please enter your name ";
+  string s;
+  cin>>s;
+  cout<<"Sorry your name is madarchod"<<endl;
+  return 0;
+  }
